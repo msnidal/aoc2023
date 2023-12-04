0453de4e57e4fae8bb04a22371006546f3c26f97
Mark Snidal <mark.snidal@gmail.com>
2023-12-03 21:25:01 -0500

    Day 2 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,14 +1,78 @@
 mod io;
 
 use io::{read_yaml, Challenge};
+use regex::Regex;
 
+struct Game {
+    id: u32,
+    rounds: Vec<Round>,
+}
+
+struct Round {
+    blue: u32,
+    red: u32,
+    green: u32,
+}
+
+impl Game {
+    fn from_str(input: &str) -> Self {
+        let game_regex = Regex::new(r"Game (\d+): (.*)").unwrap();
+        let round_regex = Regex::new(r"(\d+) (blue|red|green)").unwrap();
+
+        let caps = game_regex.captures(input).unwrap();
+        let id: u32 = caps[1].parse().unwrap();
+        let rounds_str = &caps[2];
+
+        let mut rounds = Vec::new();
+        for caps in round_regex.captures_iter(rounds_str) {
+            let count: u32 = caps[1].parse().unwrap();
+            let color = &caps[2];
+            match color {
+                "blue" => rounds.push(Round {
+                    blue: count,
+                    red: 0,
+                    green: 0,
+                }),
+                "red" => rounds.push(Round {
+                    blue: 0,
+                    red: count,
+                    green: 0,
+                }),
+                "green" => rounds.push(Round {
+                    blue: 0,
+                    red: 0,
+                    green: count,
+                }),
+                _ => (),
+            }
+        }
+
+        Self { id, rounds }
+    }
+}
 
 fn process_input(challenge: &Challenge) -> Result<u32, ()> {
-    let sum = 0;
+    let mut power_sum: u32 = 0;
     for line in challenge.input.lines() {
-        println!("{}", line)
+        let game = Game::from_str(line);
+
+        let max_blue = game
+            .rounds
+            .iter()
+            .map(|round| round.blue)
+            .max()
+            .unwrap_or(0);
+        let max_red = game.rounds.iter().map(|round| round.red).max().unwrap_or(0);
+        let max_green = game
+            .rounds
+            .iter()
+            .map(|round| round.green)
+            .max()
+            .unwrap_or(0);
+
+        power_sum += max_blue * max_red * max_green;
     }
-    Ok(sum)
+    Ok(power_sum)
 }
 
 fn main() {
